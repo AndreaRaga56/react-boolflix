58f154b990b595328505364698ae2b397f8d3fa2
AndreaRaga56 <andrearaga56@gmail.com>
2025-01-18 23:40:05 +0100

    feat page managment, corretto ottenimento dei film

--- src/App.jsx
+++ src/App.jsx
@@ -3,36 +3,52 @@ import GlobalContext from './context/GlobalContext'
 import AppLayout from './components/AppLayout'
 import HomePage from './pages/HomePage'
 import './App.css'
-import { useState } from 'react'
+import { useEffect, useState } from 'react'
 import axios from 'axios'
 const apiKey=import.meta.env.VITE_API_KEY;
-
-
-
-
 
 function App() {
   
   const apiUrl = "https://api.themoviedb.org/3/search/movie"
+  let empty ={
+    page: 0,
+    results: [],
+    total_pages: 1,
+    total_results: 0,
+  }
 
   let [searchKey, setSearchKey] = useState("")
+  let [pagina, setPagina] = useState(1)
+  let [getResp, setGetResp] = useState(empty)
 
   const handleOnChange = (event) => {
     let newSearchKey = event.target.value;
     setSearchKey(newSearchKey)
   }
 
+  const getMovies=()=>{
+    axios.get(`${apiUrl}?api_key=${apiKey}&query=${searchKey}&page=${pagina}`).then((resp) => { 
+      setGetResp(resp.data)   
+      console.log(resp.data)  
+    });    
+  }
+
+  useEffect(()=>{
+    getMovies()
+  },[pagina])
+
   const handleSubmit = (event) => {
     event.preventDefault()
-    axios.get(`${apiUrl}?api_key=${apiKey}&query=${searchKey}`).then((resp) => { 
-      console.log(resp.data)     
-    });    
+    getMovies()
   };
 
   const globalContextValue = {
     searchKey,
     handleOnChange,
-    handleSubmit
+    handleSubmit,
+    pagina,
+    setPagina,
+    getResp
   }
 
   return (
--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,8 +1,27 @@
+import { useContext, useEffect, useState } from "react";
+import GlobalContext from "../context/GlobalContext";
+
 function HomePage() {
+    const { pagina, setPagina, getResp} = useContext(GlobalContext)
+
+    const cambiaPagina = (event, x) =>{
+        event.preventDefault()
+        setPagina(pagina+x)
+    }
+
     return (
+
         <>
-        {}
+        <div>
+        Sei alla pagina {pagina} di {getResp.total_pages}
+
+        </div>
             
+            <div className="d-flex gap-2">
+                <button disabled={pagina===1} className="btn btn-secondary mt-2" onClick={(event)=>cambiaPagina(event, -1)}>Precedente</button>
+                <button disabled={pagina===getResp.total_pages} className="btn btn-secondary mt-2" onClick={(event)=>cambiaPagina(event, 1)}>Successivo</button>
+            </div>
+
         </>
     )
 }
